f9ad5c27919aa2f09b0f5f45c502485ffab6fe72
Annalisa <31178516+AnnalisaRapthap@users.noreply.github.com>
2018-09-28 10:11:24 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,10 @@
+//assignment question1
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "Enter the number:";
+	cin >> a;
+	(a>0)?cout << "The number is positive" : "The number is negative";
+}
